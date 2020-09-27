4e984238eaec4ac464b77f8b95e7952c0a1eb083
Disha Sinha <42354197+disha2sinha@users.noreply.github.com>
2020-09-27 10:53:21 +0530

    Level order traversal added

--- DATA-STRUCTURES/Tree/Traversals (Inorder, Postorder , Preorder, levelorder)/Binary Tree/Traversal_Iterative.py
+++ DATA-STRUCTURES/Tree/Traversals (Inorder, Postorder , Preorder, levelorder)/Binary Tree/Traversal_Iterative.py
@@ -40,6 +40,18 @@ def postorder(root):
             print (current.value,end=" ")
             visited.add(current)
             current=root
+def levelorder(root):
+    if root is None:
+        return
+    queue=[]
+    queue.append(root)
+    while queue:
+        current=queue.pop(0)
+        print(current.value,end=" ")
+        if current.left is not None:
+            queue.append(current.left)
+        if current.right is not None:
+            queue.append(current.right)
         
 root=Node(int(input("Enter Element: ")))
 root.left=Node(int(input("Enter Element: ")))
@@ -48,12 +60,14 @@ root.left.left=Node(int(input("Enter Element: ")))
 root.left.right=Node(int(input("Enter Element: ")))
 root.right.left=Node(int(input("Enter Element: ")))
 root.right.right=Node(int(input("Enter Element: ")))
-print("Preorder Traversal")
+print("Pre-order Traversal")
 preorder(root)
-print("\nInorder Traversal")
+print("\nIn-order Traversal")
 inorder(root)
-print("\nPostorder Traversal")
+print("\nPost-order Traversal")
 postorder(root)
+print("\nLevel-order Traversal")
+levelorder(root)
 
 
 
